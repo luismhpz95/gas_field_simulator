4a5289c4bc0c37538cafaa8a8277898be1c91d54
luismhpz95 <luismhpz95@gmail.com>
2026-06-06 17:30:15 +0300

    Fix beta value in Well class

--- src/well.py
+++ src/well.py
@@ -4,7 +4,7 @@ from src.pipe import Pipe
 
 class Well:
   
-    beta = 0.000852702
+    beta = 0.00852702
     
     def __init__(self, fluid: Fluid, k: float, h: float, re: float, rw: float, pipe: Pipe = None):
 
@@ -18,7 +18,7 @@ class Well:
     def productivity_coef(self, P_res: float, P_bhp: float) -> float:
 
         P_avg = (P_res + P_bhp)/2 
-        mu = self.fluid.mu(P_avg)
+        mu = self.fluid.mu(P_avg)  # [cP]
         C = (self.beta * self.k * self.h) / (mu * np.log(self.re / self.rw))
         return C
     
@@ -30,4 +30,3 @@ class Well:
         C = self.productivity_coef(P_res, P_bhp)
         q_std = C * (P_res - P_bhp) / self.fluid.bg(P_res)
         return q_std
-
